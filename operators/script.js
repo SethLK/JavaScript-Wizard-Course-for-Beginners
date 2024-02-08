ee8e1e137b790550439e1d51d01f42665ece25d4
SethLK <sethlk2006@gmail.com>
2024-02-08 22:34:21 +0630

    Add new

---
+++ operators/script.js
@@ -0,0 +1,58 @@
+// operators 
+/*
++
+-
+*
+/
+%
+*/
+
+// const stand for constant number
+// e, pi,
+
+// var နဲ့ let က data value ပြောင်းလို့ရတယ်
+// let sum = 3;
+// sum = sum + 5;
+
+// ဒါပေမယ့် const က ပြောင်းလို့မရ
+
+// const pi = 3.14
+// pi = 3
+
+/*
+
+ Comparison Operators: ==, ===, !=, !==, >, <, >=, <=
+တွေက ကျွန်တော်တို့ သိတဲ့အတိုင်းဘဲ compare လုပ်ပေးတာပါ
+
+ Logical Operators: &&, ||, !
+&& = and
+(true && true): true
+(true && false): false
+(false && true): false
+(false && false): false
+
+|| = or
+(false || true): true
+(true || false): true
+(true || true): true
+(false || false): false
+
+! = not
+
+ Assignment Operators: =, +=, -=, *=, /=
+ sum = 3              3 + 1
+ sum += 1 // sum  = sum + 1
+                      3 - 1
+ sum -= 1 // sum  = sum - 1
+                       3 * 1
+ sum *= 1 // sum  = sum * 1
+                       3 / 1
+ sum /= 1 // sum  = sum / 1
+
+ Assignment Operators တွေက တွက်ချက်မှုတွေကို တကြောင်းတဲ လုပ်ပေးတာပါ
+
+*/
+
+var sum = 6
+sum *= 3
+alert(sum)
---
+++ variables/script.js
@@ -0,0 +1,23 @@
+var name = "Code mal" //string
+let age = 2 //number
+const IamWatchingTHisVideo = false
+
+// window.alert// alert()
+
+// My name is name
+
+// "My name is " + name
+// Esc ‌အောက်မှာရှိပါတယ်
+// 
+/*
+    `My name is ${name}`
+*/
+
+// comments
+// '
+// ""
+// single line comment
+/*
+    double line comment
+*/
+alert('My name is \'Code mal\' ' + IamWatchingTHisVideo)
