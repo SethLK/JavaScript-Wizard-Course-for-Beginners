b6c7e1e21e81ce6130d4d25d31e7dc26b80cfd19
SethLK <sethlk2006@gmail.com>
2024-02-13 20:54:47 +0630

    Add control flow 1

---
+++ control flow/script.js
@@ -0,0 +1,80 @@
+// Control flows 
+// if - else
+// switch / case
+
+// loops
+// for , while
+
+// if else
+
+
+// does he or she know programming
+
+// 
+// var know_programming = "little"
+
+// if(know_programming == "yes"){
+//     alert("He is a programmer")
+// }
+// else if(know_programming == "little"){
+//     alert("He is learning")
+// }
+// else{
+//     alert("He isnt a programmer")
+// }
+
+// if(){
+
+// }
+// else if(){
+
+// }
+// else {
+
+// }
+
+// Ctrl + /
+
+// 1.Sunday
+// 2.Monday
+// 3.TUesday
+// 4.Wednesday
+// 5.Thursday
+// 6.Friday
+// 7.Saturday
+
+// ; ထည့်ချင်ရင် 
+// Alt + shift + f
+
+var day = 7;
+
+switch (day) {
+    case 1:
+        alert("Sunday");
+        break;
+
+    case 2:
+        alert("Monday");
+        break;
+
+    case 3:
+        alert("Tuesday");
+        break;
+
+    case 4:
+        alert("Wednesday");
+        break;
+    case 5:
+        alert("Thursday");
+        break;
+    case 6:
+        alert("Friday");
+        break;
+    case 7:
+        alert("Saturday");
+        break;
+
+    default:
+        alert("Donno");
+        break;
+}
