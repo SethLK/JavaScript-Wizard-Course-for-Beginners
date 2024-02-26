c7905ac3e7d9330463d0b3a6637c28add0927ac2
SethLK <sethlk2006@gmail.com>
2024-02-26 20:08:50 +0630

    Update Dom

---
+++ DOM/append/script.js
@@ -0,0 +1,6 @@
+const rootElement = document.querySelector("div.root")
+
+const img = document.createElement("img")
+img.setAttribute("src", "./Picture1.jpg")
+
+rootElement.appendChild(img)
---
+++ DOM/script.js
@@ -0,0 +1,29 @@
+// get element by id
+
+// const element = document.getElementById("demo")
+// // element.textContent = "Hello World"
+// element.innerHTML = "<h1>HEllo World</h1>"
+
+// get element by class
+// const element = document.getElementsByClassName("demo")[0]
+// element.innerHTML = "<h1>HEllo World by Class Name</h1>"
+
+// get element by tag name
+// const element = document.getElementsByTagName("h1")[0]
+// element.textContent = "Hello world"
+
+// const element = document.querySelector("h1")
+// element.textContent = "HEllo world from query selector"
+
+// const element = document.querySelector("h1")
+// const btn = document.querySelector("button")
+// const tomato = document.querySelector("#demo")
+
+// btn.addEventListener("click", function(){
+    
+// })
+
+// tomato.addEventListener("mouseover", function(){
+//     tomato.classList.add("blue")
+// })
+
